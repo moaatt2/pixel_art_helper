bba1aa31491b79278722f48ed754ead769542930
Dakota McKay <dakotamckay72@gmail.com>
2026-02-23 08:26:12 -0500

    Create skeleton and plan of tkinter layout.

---
+++ gui_format.py
@@ -0,0 +1,31 @@
+import tkinter
+
+################################
+### Initalize Tkinter Window ###
+################################
+
+# Initialize Tkinter Window
+window = tkinter.Tk()
+window.title("Chainmail Inlay Helper")
+window.geometry("600x600")
+
+
+######################
+### Tkinter Layout ###
+######################
+
+# TODO
+
+# Have tool bar on top (file, settings etc)
+
+# Have image preview on Right
+
+# Have Options bar, colapsable sections on left
+
+
+#######################
+### Start Main Loop ###
+#######################
+
+# Start window
+window.mainloop()
