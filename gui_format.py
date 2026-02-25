a6c284ea5f01a237afe346277d31febb8ce8cb8c
Dakota McKay <dakotamckay72@gmail.com>
2026-02-25 08:17:34 -0500

    Add a basic menu bar.

--- gui_format.py
+++ gui_format.py
@@ -10,6 +10,25 @@ window.title("Chainmail Inlay Helper")
 window.geometry("600x600")
 
 
+################
+### Menu Bar ###
+################
+
+# Initalize menu bar
+menu_bar = tkinter.Menu(window)
+
+# Add file menu
+file_menu = tkinter.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Open", command=None)
+file_menu.add_command(label="Save", command=None)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=window.quit)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+# Add menu bar to window
+window.config(menu=menu_bar)
+
+
 ######################
 ### Tkinter Layout ###
 ######################
