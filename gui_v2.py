5e49f0f5e6e40e84d0f7d04b33922ecd3bc07bc0
Dakota McKay <dakotamckay72@gmail.com>
2026-03-29 17:38:28 -0400

    Set window title and add an element.

--- gui_v2.py
+++ gui_v2.py
@@ -1,8 +1,25 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
+# Setup class for main window
+class main_window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Pixel Art Helper")
+
+        button = QPushButton("Click me")
+
+        self.setCentralWidget(button)
+
+
+# Create application instance
 app = QApplication([])
 
-window = QWidget()
+# Create an instance of the main window
+window = main_window()
+
+# Show the main window instance
 window.show()
 
+# Start the application's event loop
 app.exec()
